f03a2529d2fcf03114536293d42d20beebe0fad4
patrykn01 <patryk.niemczyk12345@gmail.com>
2024-12-28 14:28:29 +0100

    Dodanie skryptu zapisującego preferencje użytkownika

--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,23 @@
 const themeToggleButton = document.getElementById("theme-toggle");
 
-		themeToggleButton.addEventListener("click", () => {
+// Funkcja do ustawiania motywu
+function setTheme(theme) {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme); // Zapisanie motywu w localStorage
+}
 
-			if (document.documentElement.getAttribute("data-theme") === "dark") {
-				document.documentElement.setAttribute("data-theme", "light");
-			}
+// Pobranie motywu z localStorage przy ładowaniu strony
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme) {
+    setTheme(savedTheme);
+} else {
+    // Jeśli brak zapisu, ustaw domyślny motyw
+    const defaultTheme = "light";
+    setTheme(defaultTheme);
+}
 
-			else {
-				document.documentElement.setAttribute("data-theme", "dark");
-			}
-		});
+themeToggleButton.addEventListener("click", () => {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    setTheme(newTheme);
+});
